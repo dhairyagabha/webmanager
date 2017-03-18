d17eb617b4ba53f887f9ad252b9451fc2aa32083
Dhairya Gabhawala <gabhawaladhairya@gmail.com>
2017-03-18 00:28:32 -0500

    built external routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Webmanager::Engine.routes.draw do
+  root to: 'home#index'
   devise_for :users, class_name: "Webmanager::User", :module => :devise
   devise_scope :user do
     get 'sign_in', to: '/devise/sessions#new'
     delete 'sign_out', to: '/devise/sessions#destroy'
   end
-  root to: 'home#index'
+  Webmanager::DynamicRouter.load
   resources :articles
   resources :forms
   resources :form_fields
