3ea00a0990e44357f5961e61721fff9824763e77
Dhairya Gabhawala <gabhawaladhairya@gmail.com>
2017-04-08 15:48:40 -0500

    select2

--- app/assets/javascripts/webmanager/custom.js
+++ app/assets/javascripts/webmanager/custom.js
@@ -10,13 +10,8 @@ $(document).ready(function () {
         placeholder: 'Add Article Tags ...',
         tags: true,
         tokenSeparators: [',', ' ']
-        // templateSelection: formatSelection
     })
 });
-
-function remove_tag(){
-    console.log($(this).parent());
-}
 
 function remove_fields(link) {
     $(link).prev("input[type=hidden]").val(true);
@@ -28,34 +23,3 @@ function add_fields(link, association, content) {
     var regexp = new RegExp("new_" + association, "g");
     $('.new-fields').append(content.replace(regexp, new_id));
 }
-
-
-
-
-
-// var tag_list = $('#article_tag_list').val().split(',');
-// var tags = [ ];
-// for(i = 0; i < tag_list.length; i++){
-//     tags.push({tag: tag_list[i]});
-// }
-// $('.chips').material_chip({
-//     placeholder: 'Enter a tag',
-//     secondaryPlaceholder: '+Tag',
-//     data: tags
-// });
-// $('.chips').on('chip.add', function (e, chip) {
-//     console.log(this.material_chip(data));
-//     var field = $('#article_tag_list');
-//     var tags = null;
-//     if (field.val() == null) {
-//         tags = $('#article_tag_list').val() + chip.tag + ',';
-//     } else {
-//         tags = $('#article_tag_list').val() + ',' + chip.tag;
-//     }
-//     $('#article_tag_list').val(tags);
-// });
-
-// function chip_data(item, index){
-//     var data = '{tag: item}';
-//     return data
-// }
