3641edebcccf26d90d4de71c72d6c6d429cd07d7
Dhairya Gabhawala <dhairya@rev365.com>
2017-07-23 10:40:23 -0500

    asset permits word docs, pdf, and images

--- app/models/webmanager/blog_asset.rb
+++ app/models/webmanager/blog_asset.rb
@@ -2,14 +2,18 @@ module Webmanager
   class BlogAsset < ApplicationRecord
     has_many :assets, dependent: :destroy
     has_many :articles, through: :assets
-    has_attached_file :asset, styles: Webmanager.image_styles, default_url: "/images/:style/missing.png"
+    has_attached_file :asset, styles: Webmanager.image_styles
     validates_attachment_content_type :asset,
                                       :content_type => %w(image/jpeg image/jpg image/png image/gif application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document),
-    :message => 'only (png/gif/jpeg) images'
+                                      :message => 'only (png/gif/jpeg) images'
 
+    before_post_process :skip_for_files
     before_save :extract_dimensions
     serialize :dimensions
 
+    def skip_for_files
+      ! %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document).include?(asset_content_type)
+    end
     def image?
       asset_content_type =~ %r{^(image|(x-)?application)/(bmp|gif|jpeg|jpg|pjpeg|png|x-png)$}
     end
